a82e2707ab8af8444e29913776fbe63fd9c8ec0e
EmilMajid <emilinpoctu@gmail.com>
2025-05-08 21:13:42 +0400

    12

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
+for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print("{}{}".format (i,j), end=", ")
-        elif i == 8 and j == 9:
-            print("{}{}".format (i,j))            
+            print("{}{}".format(i, j), end=", ")
         else:
-            print("\n")
+            print("{}{}".format(i, j))
