ed7a5c1f092d5c9bf7e63e0e3fb05bb7622999c0
EmilMajid <emilinpoctu@gmail.com>
2025-05-10 17:13:20 +0000

    1

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
-    hidden_names = dir(hidden_4)
-    for i in hidden_names:
-        if i.startswith("__") == False:
-            print(i)
+    names = dir(hidden_4)
+    for name in sorted([name for name in names if not name.startswith("__")]):
+        print(name)
