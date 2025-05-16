3ca1d24eb70731b6f52cfa24a6eb1420a613505b
EmilMajid <emilinpoctu@gmail.com>
2025-05-16 19:59:59 +0400

    2-1

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_numbers = set(my_list)  # Siyahını toplusuna (set) çevir – təkrarlananlar silinir
+    unique_numbers = set(my_list)
     result = 0
     for number in unique_numbers:
         result += number
