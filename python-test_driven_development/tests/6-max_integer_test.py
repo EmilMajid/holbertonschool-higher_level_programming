25eb6f893c631e7b9df6e5a7d699540063e786fc
EmilMajid <emilinpocti@gmail.com>
2025-05-19 17:45:54 +0000

    test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Module to test max_integer() function
+'''
+
+import unittest
+from 6-max_integer import max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    '''
+    Class for testing max_integer() function with two methods
+    '''
+    def test_max(self):
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1,2,3,4,5]), 5)
+        self.assertAlmostEqual(max_integer([10,2,24,52,1]), 52)
+        self.assertAlmostEqual(max_integer([-1,-2,-3,-4,-5]), -1)
+        self.assertAlmostEqual(max_integer([-1,-2,-3,-4, 0]), 0)
+        self.assertAlmostEqual(max_integer([1,2,3,-4,-5]), 3)
+
+    def test_values(self):
+        self.assertRaises(TypeError, max_integer('a'), 'argument must be a list')
+        self.assertRaises(TypeError, max_integer(['a', 1, 2, 3]), ''>' not supported between instances of 'int' and 'str'')
