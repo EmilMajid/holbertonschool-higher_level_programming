feedb8159fc8254da0a695d41142e6ba132d174e
EmilMajid <emilinpocti@gmail.com>
2025-05-19 11:17:16 +0000

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -17,6 +17,9 @@ def matrix_divided(matrix, div):
     for sublist in matrix:
         if len(sublist) != max_len:
             raise TypeError("Each row of the matrix must have the same size")
+            for element in sublist:
+                if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         new_matrix.append(sublist[:])
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
