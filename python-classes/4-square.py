da980c562705c3bf0f7035dd22581ce585fc3121
EmilMajid <emilinpoctu@gmail.com>
2025-05-20 17:15:36 +0000

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -23,14 +23,30 @@ class Square:
         '''
         return self.__size ** 2
 
+    @property
     def size(self):
         '''
         it is getter for size
         '''
         return self.__size
 
+    @size.setter
     def size(self, value):
         '''
         this is setter for size
         '''
         self.__size = value
+
+
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
