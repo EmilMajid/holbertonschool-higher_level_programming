4f21efa833f0a33b3598af68946c0ebf98dbe088
EmilMajid <emilinpoctu@gmail.com>
2025-05-20 16:45:03 +0000

    3-square.py1

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''
-this module 1-square right now it is empty
+this module 3-square right now it is empty
 '''
 
 
 class Square:
     '''
-    square class has private attr size
+    square class has private attr size returns the current square area
     '''
     def __init__(self, size=0):
         if not isinstance(size, int):
