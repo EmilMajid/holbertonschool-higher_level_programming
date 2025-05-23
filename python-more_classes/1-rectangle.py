c516dcc21bb53d9e045b9dfbcc20833bc461f042
EmilMajid <emilinpoctu@gmail.com>
2025-05-23 15:42:50 +0000

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")  
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -37,4 +37,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
