4ba0c472a0e16761c1087a2339433b4736f3fd10
EmilMajid <emilinpoctu@gmail.com>
2025-05-31 13:03:12 +0000

    123

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,7 +1,36 @@
-class SeaSerpent(SwimMixin):
-    def hiss(self):
-        print("The sea serpent hisses!")
+class SwimMixin:
+    """
+    Mixin class that provides swimming ability.
+    This class is intended to be used alongside other classes
+    to add the swim() method functionality.
+    """
+    def swim(self):
+        """
+        Print a message indicating that the creature can swim.
+        """
+        print("The creature swims!")
 
-class Griffin(FlyMixin):
-    def screech(self):
-        print("The griffin screeches!")
+
+class FlyMixin:
+    """
+    Mixin class that provides flying ability.
+    This class is intended to be used alongside other classes
+    to add the fly() method functionality.
+    """
+    def fly(self):
+        """
+        Print a message indicating that the creature can fly.
+        """
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    A Dragon class that can swim and fly by inheriting from
+    SwimMixin and FlyMixin. It also has its own behavior like roaring.
+    """
+    def roar(self):
+        """
+        Print a message indicating that the dragon is roaring.
+        """
+        print("The dragon roars!")
