aec78e04067f7f04631f2a9f11edb896fb84e074
EmilMajid <emilinpoctu@gmail.com>
2025-06-17 14:18:34 +0000

    9-5

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,7 +5,6 @@
 class Student:
     """Represents a student with basic information and JSON serialization."""
 
-
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student instance.
         Args:
@@ -16,7 +15,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Retrieves a dictionary representation of the Student instance.
