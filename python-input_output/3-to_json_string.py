d511a035e513e8ad69b57cd5a9eee1d87a37d470
EmilMajid <emilinpoctu@gmail.com>
2025-06-17 13:43:57 +0000

    3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for converting Python objects to JSON strings."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object as a string.
+        Args:
+        my_obj: The Python object to convert to JSON
+    Returns:
+        str: The JSON string representation of the object
+    Note:
+        Will raise TypeError if the object contains non-serializable types
+    """
+    
+    return json.dumps(my_obj)
