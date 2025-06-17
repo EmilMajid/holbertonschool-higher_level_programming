6cd54a8cf93eab3f8ef4ce92aabe296a8d2958db
EmilMajid <emilinpoctu@gmail.com>
2025-06-17 14:12:57 +0000

    9-2

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,12 +7,12 @@ class Student:
     
 
     def __init__(self, first_name, last_name, age):
-    """Initializes a new Student instance.
+        """Initializes a new Student instance.
     Args:
     first_name (str): The student's first name
     last_name (str): The student's last name
     age (int): The student's age
-    """
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
