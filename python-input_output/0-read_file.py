4f6a59cbdd281e296cb94b06d3f188cb28923c94
EmilMajid <emilinpoctu@gmail.com>
2025-06-17 13:23:16 +0000

    0-2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading files"""
+
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
+    """Reads a text file (UTF8) and prints it to stdout.
 
     Args:
         filename (str): The name of the file to read. Defaults to "".
