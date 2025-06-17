d7b6b7b4ce8147bd3af379c97b013bd010283d48
EmilMajid <emilinpoctu@gmail.com>
2025-06-17 13:19:51 +0000

    0-1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
@@ -10,4 +11,3 @@ def read_file(filename=""):
     """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
-        
