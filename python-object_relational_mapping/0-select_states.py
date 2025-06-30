734cc31237bb7c085a64d5491f7116c339b4362c
EmilMajid <emilinpoctu@gmail.com>
2025-06-30 18:22:30 +0400

    bezdim

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script connects to a MySQL database and lists all states
-from the 'states' table in the specified database, ordered by id in ascending order.
+from the 'states' table in the specified database, ordered 
+by id in ascending order.
 It takes three command-line arguments: username, password, and database name.
 """
 import MySQLdb
