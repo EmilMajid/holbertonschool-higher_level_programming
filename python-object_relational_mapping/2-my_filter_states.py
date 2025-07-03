c210028b3bf983e3788c7c95c117e72b97391c00
EmilMajid <emilinpoctu@gmail.com>
2025-07-03 14:58:52 +0400

    11

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a name matching the argument.
-"""
+"""Module for Selecting states where name equals argument"""
 
-import MySQLdb
-import sys
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
     db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_name
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
-    cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}'"
-    "ORDER BY id ASC".format(state_name)
-    cur.execute(query)
 
-    results = cur.fetchall()
-    for row in results:
-        print(row)
+    cursor = db.cursor()
 
-    cur.close()
-    db.close()
+    cursor.execute("SELECT * \
+                    FROM `states` \
+                    WHERE BINARY `name` = '{}' \
+                    ORDER BY id".format(argv[4]))
+
+    for state in cursor.fetchall():
+        print(state)
+
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
